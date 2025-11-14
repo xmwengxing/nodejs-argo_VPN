c0d06296b0d0463d670a07cc737b584095a1a0f5
世敬天 <123044948+xmwengxing@users.noreply.github.com>
2025-11-14 18:10:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'sd.shijingtian.dpdns.org';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMjM2NjZkNTQxMDYyMWY5NDVlMjJlOTM4YjY4NDVmZWYiLCJ0IjoiODIxOGZlZGYtZWNjNy00ZDJhLWFiMGUtMTNiZmI2Y2I5ZjdiIiwicyI6Ik5EWmpZekkzWVRjdFpqVXpZUzAwT0RCaUxUbGpOMll0WVRka09UQmtZelpqWm1VdyJ9
-';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMjM2NjZkNTQxMDYyMWY5NDVlMjJlOTM4YjY4NDVmZWYiLCJ0IjoiODIxOGZlZGYtZWNjNy00ZDJhLWFiMGUtMTNiZmI2Y2I5ZjdiIiwicyI6Ik5EWmpZekkzWVRjdFpqVXpZUzAwT0RCaUxUbGpOMll0WVRka09UQmtZelpqWm1VdyJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
